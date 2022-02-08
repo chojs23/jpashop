d28c23225a19f4bc349ccbbee457101112d8eb15
chojs23 <chojs990222@gmail.com>
2022-02-08 22:17:32 +0900

    Create Entity Class

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+    @OneToMany(mappedBy = "member") //일대다 관계 //Order table 에 있는 member 필드에 의해 mapping 됨
+    //연관관계 주인 아님 맵핑된 애
+    private List<Order> orders = new ArrayList<>();
+
+}
