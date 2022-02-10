3a4216941e90ec48e3816f869582266e7c8338bf
chojs23 <chojs990222@gmail.com>
2022-02-10 22:32:25 +0900

    Member domain

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -15,16 +15,16 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne() //Member 랑 다대일 관계
+    @ManyToOne(fetch=FetchType.LAZY) //Member 랑 다대일 관계 //X to one 은 FetchType.Lazy로 다 바꾸어야됨
     @JoinColumn(name = "member_id")//Foreign key
     private Member member; //주문 회원
 
 
-    @OneToMany(mappedBy = "order")//
+    @OneToMany(mappedBy = "order" ,cascade = CascadeType.ALL)// X to Many 는 기본 FetchType.Lazy
     private List<OrderItem> orderItems = new ArrayList<>();
 
 
-    @OneToOne()
+    @OneToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery; //배송정보
 
@@ -33,6 +33,9 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+
+
     //==연관관계 메서드==//
     public void setMember(Member member) {
         this.member = member;
